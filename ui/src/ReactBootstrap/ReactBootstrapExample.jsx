3c2b7ce9d9bc929134e328c2fe8221bacfbb9cb9
rondido <rppr001@naver.com>
2022-11-30 23:37:21 +0900

    React Bootstrap commit

--- ui/src/ReactBootstrap/ReactBootstrapExample.jsx
+++ ui/src/ReactBootstrap/ReactBootstrapExample.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 import AlertDismissible from './AlertDismissible'
 import ButtonExample from './ButtonExample'
+import CarouselExample from './CarouselExample'
 import GridExample from './GridExample'
+import MyModalExample from './MyModalExample'
 
 export default function ReactBootstrapExample() {
   return (
     <div>
+      <MyModalExample />
+        <br/>
+        <CarouselExample />
         <ButtonExample/>
         <br/>
         <GridExample/>
