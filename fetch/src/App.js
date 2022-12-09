b207fb275f0ae3f52201f28d5aff95cfd128dae3
rondido <rppr001@naver.com>
2022-12-09 21:36:38 +0900

    React 상태관리라이브러리(zustand) install and input commit

--- fetch/src/App.js
+++ fetch/src/App.js
@@ -1,16 +1,18 @@
 import logo from "./logo.svg";
 import "./App.css";
+import Text from "./components/ZustandExample/Text";
+import CharacterCounter from "./components/ZustandExample/CharacterCounter";
 // import TextMocking from "./components/TextMocking";
 // import Counter from "./features/counter/Counter";
 // import MobxExample from "./components/MobxExample";
 // import TodoList from "./components/TodoList";
 // import { observableTodoStore } from "./app/ObservableTodoStore";
-import { RecoilRoot } from "recoil";
-import FontButton from "./components/RecoilExeample/FontButton";
-import Text from "./components/RecoilExeample/Text";
-import CharacterCounter from "./components/RecoilExeample/CharacterCounter";
-import CurrentUserInfo from "./components/RecoilExeample/CurrentUserInfo";
-import TodoList from "./components/RecoilExeample/Todo/TodoList";
+// import { RecoilRoot } from "recoil";
+// import FontButton from "./components/RecoilExeample/FontButton";
+// import Text from "./components/RecoilExeample/Text";
+// import CharacterCounter from "./components/RecoilExeample/CharacterCounter";
+// import CurrentUserInfo from "./components/RecoilExeample/CurrentUserInfo";
+// import TodoList from "./components/RecoilExeample/Todo/TodoList";
 function App() {
   return (
     <div className="App">
@@ -18,13 +20,15 @@ function App() {
       <Counter /> */}
       {/* <MobxExample /> */}
       {/* <TodoList store={observableTodoStore} /> */}
-      <RecoilRoot>
-        {/* <FontButton />
+      {/* <RecoilRoot> */}
+      {/* <FontButton />
         <Text />
         <CharacterCounter />
         <CurrentUserInfo /> */}
-        <TodoList />
-      </RecoilRoot>
+      {/* <TodoList /> */}
+      {/* </RecoilRoot> */}
+      {/* <Text /> */}
+      <CharacterCounter />
     </div>
   );
 }
