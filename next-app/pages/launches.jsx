786e94947baf78d2bc631cac9df96a7847cea6e7
rondido <rppr001@naver.com>
2022-12-27 15:23:40 +0900

    Next.js 프로젝트 생성 및 초기 환경 세팅 commit

---
+++ next-app/pages/launches.jsx
@@ -0,0 +1,35 @@
+//import React, { useState,useEffect } from 'react'
+
+export default function Launches({data}) {
+    // const [data,setData ]= useState(null);
+    // useEffect(()=>{
+    //     const fetchLaunches = async () => {
+    //         const res = await fetch("https://api.spacexdata.com/v3/launches");
+    //         const data =  await res.json();
+    //         setData(data);
+    //     }
+    //     fetchLaunches();
+    // },[]);
+
+    if(data == null){
+        return null;
+    }
+  return (
+    <div>
+        <ul>
+            {data.map((launch,index)=>(
+                <li key={index}>{index +1}.{launch.mission_name}</li>
+            ))}
+        </ul>
+    </div>
+  )
+}
+
+export async function getServerSideProps() {
+    const res = await fetch("https://api.spacexdata.com/v3/launches");
+    const data =  await res.json();
+    console.log('getServerSideProps');
+    return {
+      props: {data}, // will be passed to the page component as props
+    }
+  }
